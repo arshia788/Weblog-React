6f4ee2b3970451cb450a2368797cd2e2f4fae85a
Amirali Amanipour <arshia.ala80@gmail.com>
2023-06-26 11:32:27 +0330

    wokred on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <nav>
-            
+        <nav className='flex justify-center
+         items-center py-4 px-10 bg-gray-800 text-white'>
+            <div className='flex justify-between items-center w-1/2 '>
+
+                <h1 className='text-3xl font-semibold '>Weblog</h1>
+
+                <ul className='flex items-center gap-x-3 text-lg'>
+                    <li>home</li>
+                    <li>search</li>
+                    <li>log-in</li>
+                    <li>sign-up</li>
+                </ul>
+            </div>
         </nav>
     );
 };
